c84de2278f5970fe1c7d0bb6e7c192e002170496
Miguel <miguelqnicolas@gmail.com>
2020-03-15 22:22:39 -0400

    started on passport setup

---
+++ config/passport-setup.js
@@ -0,0 +1,6 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
+const User = require('../auth/user-model');
+
