88a44f988a43759ea6a74606f01b867691afe9d0
Miguel <miguelqnicolas@gmail.com>
2020-03-29 21:33:36 -0400

    completed twitter authentication and common endpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,16 +1,44 @@
 const express = require('express');
-const User = require('./user-model');
+const passport = require('passport');
+const User = require('./auth-model');
 
 const app = express.Router();
 
-// register
-app.post('/register', (request, response) => {
-    User.register(request.body)
-        .then(res => response.status(200).json({message: 'registered successfully'}))
-        .catch(err => {
-            response.status(500).json({message: 'error registering user'});
-            console.log(error);
-        });
+//twitter
+app.get('/twitter', passport.authenticate('twitter'));
+
+app.get('/twitter/redirect', passport.authenticate('twitter', { failureRedirect: 'https//localhost:3000/error' }), (request, response) => {
+    response.redirect('http://localhost:3000/success');
+});
+
+// common
+app.get('/success', (request, response) => {
+	response.status(200).json({
+		message: 'successfully fetched user object',
+		user: {
+            id: request.user.id,
+            displayName: request.user.display_name,
+            profilePicture: request.user.profile_picture,
+            role: request.user.role,
+            cohort: request.user.cohort
+        }
+	});
+});
+
+app.get('/logout', (request, response) => {
+	request.logout();
+
+	if (request.session) {
+		request.session.destroy(err => {
+			if (err) {
+				response.status(500).json({ message: 'error destroying session' });
+			} else {
+				response.status(200).clearCookie('viewee').json({ message: 'successfully signed out' });
+			}
+		});
+	} else {
+		response.status(204).json({ message: 'session does not exist' });
+	};
 });
 
 module.exports = app;
