fabb4f09b5a6ebfdff077371cadc75dd2b18e649
Miguel <miguelqnicolas@gmail.com>
2020-04-02 14:44:11 -0400

    updated the post endpoints and model helper functions

--- posts/post-model.js
+++ posts/post-model.js
@@ -5,7 +5,22 @@ const create = post => {
 };
 
 const fetch = () => {
-    return database('posts');
+    return database('posts')
+        .join('users', 'posts.user_id', 'users.id')
+        .select([
+            'posts.id',
+            'users.id as user_id',
+            'users.profile_picture',
+            'users.display_name',
+            'posts.track',
+            'posts.category',
+            'posts.question',
+            'posts.answer',
+            'posts.likes',
+            'posts.comments',
+            'posts.created_at',
+            'posts.updated_at'
+        ]);
 };
 
 module.exports = {
--- posts/post-router.js
+++ posts/post-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../auth/auth-model');
 const Post = require('./post-model');
 
 const app = express.Router();
@@ -6,16 +7,16 @@ const app = express.Router();
 // create post
 app.post('/', (request, response) => {
     Post.create({
-        user_id: request.params.id,
+        user_id: request.user.id,
         question: request.body.question,
         answer: request.body.answer,
-        cohort: request.body.cohort,
+        track: request.body.track,
         category: request.body.category
     })
         .then(res => response.status(200).json({message: 'post created successfully'}))
         .catch(err => {
-            response.status(500).json({message: 'error creating post'});
             console.log(err);
+            response.status(500).json({message: 'error creating post'});
         });
 });
 
@@ -24,8 +25,8 @@ app.get('/', (request, response) => {
     Post.fetch()
         .then(res => response.status(200).json(res))
         .catch(err => {
-            response.status(500).json({message: 'error fetching posts'});
             console.log(err);
+            response.status(500).json({message: 'error fetching posts'});
         });
 });
 
