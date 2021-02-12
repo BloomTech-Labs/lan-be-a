45d89695d412bb8553d93d6f39d99295c7ff99b3
salza323 <sal.zamora480@gmail.com>
2021-02-12 14:41:24 -0700

    added prettier and eslint to package.json and fixed passport-setup

--- config/passportSetup.js
+++ config/passportSetup.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const LinkedInStrategy = require('passport-linkedin').Strategy;
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 // const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // const FacebookStrategy = require('passport-facebook').Strategy;
@@ -9,16 +9,15 @@ const User = require('../models/user');
 const BACKEND_URL = process.env.BACKEND_DEPLOYED_URL || 'http://localhost:5000';
 
 passport.serializeUser((user, done) => {
-    console.log('serializeUser', user);
-    done(null, user.id);
+  console.log('serializeUser', user);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    console.log('deserializeUser', id);
-    User.find({ id: id })
-        .then(user => {
-            done(null, user);
-        });
+  console.log('deserializeUser', id);
+  User.find({ id: id }).then((user) => {
+    done(null, user);
+  });
 });
 
 // Found online, might be it
@@ -27,64 +26,70 @@ passport.deserializeUser((id, done) => {
 //     done(null, user)
 //  })
 
-passport.use(new LinkedInStrategy({
-    consumerKey: process.env.LINKEDIN_CLIENT_ID,
-    consumerSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    callbackURL: `${BACKEND_URL}/api/auth/linkedin/redirect`
-  }, (accessToken, refreshToken, profile, done) => {
-        console.log('LinkedIn strategy hit', profile);
-        const user = {
-            id: profile.id,
-            email: profile.emails[0].value,
-            display_name: profile.displayName,
-            profile_picture: profile.photos[3].value
-        };
+// passport.use(
+//   new LinkedInStrategy(
+//     {
+//       consumerKey: process.env.LINKEDIN_CLIENT_ID,
+//       consumerSecret: process.env.LINKEDIN_CLIENT_SECRET,
+//       callbackURL: `${BACKEND_URL}/api/auth/linkedin/redirect`,
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log('LinkedIn strategy hit', profile);
+//       const user = {
+//         id: profile.id,
+//         email: profile.emails[0].value,
+//         display_name: profile.displayName,
+//         profile_picture: profile.photos[3].value,
+//       };
 
-        User.find({ id: user.id })
-            .then(existingUser => {
-                console.log('existingUser', existingUser);
-                if (existingUser) {
-                    return done(null, existingUser);
-                } else {
-                    User.add(user)
-                        .then(newUser => {
-                            console.log('newUser', newUser)
-                            return done(null, newUser[0]);
-                        });
-                };
+//       User.find({ id: user.id }).then((existingUser) => {
+//         console.log('existingUser', existingUser);
+//         if (existingUser) {
+//           return done(null, existingUser);
+//         } else {
+//           User.add(user).then((newUser) => {
+//             console.log('newUser', newUser);
+//             return done(null, newUser[0]);
+//           });
+//         }
+//       });
+//     }
+//   )
+// );
+
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      callbackURL: `${BACKEND_URL}/api/auth/linkedin/redirect`,
+      scope: ['r_emailaddress', 'r_liteprofile'],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('LinkedIn strategy hit', profile);
+      const user = {
+        id: profile.id,
+        email: profile.emails[0].value,
+        display_name: profile.displayName,
+        profile_picture: profile.photos[3].value,
+      };
+
+      process.nextTick(() => {
+        User.find({ id: user.id }).then((existingUser) => {
+          console.log('existingUser', existingUser);
+          if (existingUser) {
+            return done(null, existingUser);
+          } else {
+            User.add(user).then((newUser) => {
+              console.log('newUser', newUser);
+              return done(null, newUser[0]);
             });
-}));
-
-// passport.use(new LinkedInStrategy({
-//     clientID: process.env.LINKEDIN_CLIENT_ID,
-//     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-//     callbackURL: `${BACKEND_URL}/api/auth/linkedin/redirect`,
-//     scope: ['r_emailaddress', 'r_liteprofile'],
-//   }, (accessToken, refreshToken, profile, done) => {
-//         console.log('LinkedIn strategy hit', profile);
-//         const user = {
-//             id: profile.id,
-//             email: profile.emails[0].value,
-//             display_name: profile.displayName,
-//             profile_picture: profile.photos[3].value
-//         };
-
-//         process.nextTick(() => {
-//             User.find({ id: user.id })
-//                 .then(existingUser => {
-//                     console.log('existingUser', existingUser);
-//                     if (existingUser) {
-//                         return done(null, existingUser);
-//                     } else {
-//                         User.add(user)
-//                             .then(newUser => {
-//                                 console.log('newUser', newUser)
-//                                 return done(null, newUser[0]);
-//                             });
-//                     };
-//                 });
-//         });
-// }));
+          }
+        });
+      });
+    }
+  )
+);
 
 // passport.use(new LinkedInStrategy({
 //     clientID: process.env.GOOGLE_CLIENT_ID,
