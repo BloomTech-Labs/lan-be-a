f3be009378d17aa1e14bda88172c74d0fc849217
justinmrks <justinmarks19@gmail.com>
2021-02-18 16:15:36 -0600

    Added endpoints for pulling posts for a room

--- routes/room-route.js
+++ routes/room-route.js
@@ -1,27 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express.Router();
 
-const Room = require('../models/room-model');
+const Room = require("../models/room-model");
 
-
-app.get('/', (req, res) => {
-    Room.getAllRooms()
-        .then((rooms) => {
-            res.status(200).json(rooms);
-        })
-        .catch(() => {
-            res.status(500).json({ message: 'Could not retrieve rooms' });
-        });
+app.get("/", (req, res) => {
+  Room.getAllRooms()
+    .then((rooms) => {
+      res.status(200).json(rooms);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Could not retrieve rooms" });
+    });
 });
 
 //create a room if users role is 3(admin) and verify that room_name is in req
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   const { role_id } = req.user;
   const { room_name } = req.body;
   if (role_id != 3) {
-    res.status(403).json({ message: 'Access denied.' });
+    res.status(403).json({ message: "Access denied." });
   } else if (!room_name) {
-    res.status(400).json({ message: 'Must designate room name to continue.' });
+    res.status(400).json({ message: "Must designate room name to continue." });
   } else {
     Room.add(req.body)
       .then((data) => {
@@ -34,12 +33,12 @@ app.post('/', (req, res) => {
 });
 
 //delete a room if users role is 3(admin)
-app.delete('/:id', (req, res) => {
+app.delete("/:id", (req, res) => {
   const { role_id } = req.user;
   const { roomId } = req.params.id;
 
   if (role_id != 3) {
-    res.status(403).json({ message: 'Access denied.' });
+    res.status(403).json({ message: "Access denied." });
   } else {
     Room.remove(roomId)
       .then((numberOfRoomsToRemove) => {
@@ -51,6 +50,38 @@ app.delete('/:id', (req, res) => {
         res.status(500).json({ message: err.message });
       });
   }
+});
+
+app.get("/:id/recent", (request, response) => {
+  console.log(request.params);
+  Room.fetchRecentByRoomId(request.params.id)
+    .then((data) => response.status(200).json(data))
+    .catch((err) =>
+      response.status(400).json({
+        message: `Failed to fetch all posts for room with ID:${request.params.id}`,
+        error: err,
+      })
+    );
+});
+
+app.get("/:id/popular", (request, response) => {
+  Room.fetchPopularByRoomId(request.params.id)
+    .then((data) => response.status(200).json(data))
+    .catch(() =>
+      response.status(400).json({
+        message: `Failed to fetch all posts for room with ID:${request.params.id}`,
+      })
+    );
+});
+
+app.get("/:id/search", (request, response) => {
+  Room.fetchPopularByRoomId(request.params.id)
+    .then((data) => response.status(200).json(data))
+    .catch(() =>
+      response.status(400).json({
+        message: `Failed to fetch all posts for room with ID:${request.params.id}`,
+      })
+    );
 });
 
 module.exports = app;
