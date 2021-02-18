7c0ef90d97a2960e95254d53f86979b5c4436eb8
James Lundin <info@thecodediver.com>
2021-02-18 09:47:10 -0500

    changed double quotes to single quotes to keep project convention

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const passport = require("passport");
-const session = require("express-session");
-const passportSetup = require("./config/passportSetup");
-const knexSessionStore = require("connect-session-knex")(session);
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const passport = require('passport');
+const session = require('express-session');
+const passportSetup = require('./config/passportSetup');
+const knexSessionStore = require('connect-session-knex')(session);
 
-const config = require("./database/dbConfig");
-const authRouter = require("./routes/auth");
-const userRouter = require("./routes/user");
-const postRouter = require("./routes/post");
-const commentRouter = require("./routes/comment");
-const roomRouter = require("./routes/room-route");
-const adminRouter = require("./routes/admin");
+const config = require('./database/dbConfig');
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const postRouter = require('./routes/post');
+const commentRouter = require('./routes/comment');
+const roomRouter = require('./routes/room-route');
+const adminRouter = require('./routes/admin');
 
-const User = require("./models/user");
+const User = require('./models/user');
 
 const app = express();
 
 const FRONTEND_URL =
-  process.env.FRONTEND_DEPLOYED_URL || "http://localhost:3000";
+  process.env.FRONTEND_DEPLOYED_URL || 'http://localhost:3000';
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -59,21 +59,21 @@ async function verifyRole(req, res, next) {
       req.user.role_id = verifiedUser.role_id;
       return next();
     }
-    return res.status(401).send("User does not exist");
+    return res.status(401).send('User does not exist');
   } catch (error) {
-    return res.status(500).send("Database error");
+    return res.status(500).send('Database error');
   }
 }
 
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRouter);
-app.use("/api/post", verifyRole, postRouter);
-app.use("/api/comment", verifyRole, commentRouter);
-app.use("/api/room", verifyRole, roomRouter);
-app.use("/api/admin", verifyRole, adminRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/post', verifyRole, postRouter);
+app.use('/api/comment', verifyRole, commentRouter);
+app.use('/api/room', verifyRole, roomRouter);
+app.use('/api/admin', verifyRole, adminRouter);
 
-app.get("/", (request, response) =>
-  response.send({ message: "Server working" })
+app.get('/', (request, response) =>
+  response.send({ message: 'Server working' })
 );
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
