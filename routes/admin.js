eaa79003559b72a03e6ffd06d414dfffa736fd2d
James Lundin <info@thecodediver.com>
2021-02-18 17:02:02 -0500

    added role_id check to get user endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -4,21 +4,29 @@ const Admin = require('../models/admin');
 const app = express.Router();
 
 app.get('/users', async (req, res) => {
-  try {
-    const data = await Admin.getUsers();
-    res.status(200).json(data);
-  } catch(err) {
-    res.status(500).json({ message: err.message });
+  if (req.user.role_id === 3) {
+    try {
+      const data = await Admin.getUsers();
+      res.status(200).json(data);
+    } catch(err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(400).json({ message: "You do not have the correct role to do this" });
   }
 });
 
 app.get('/rooms', async (req, res) => {
-  try {
-    const data = await Admin.getRooms();
-    res.status(200).json(data);
-  } catch(err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
+  if (req.user.role_id === 3) {
+    try {
+      const data = await Admin.getRooms();
+      res.status(200).json(data);
+    } catch(err) {
+      console.log(err);
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(400).json({ message: "You do not have the correct role to do this" });
   }
 });
 
