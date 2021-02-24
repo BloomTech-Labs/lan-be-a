4d015c18de460482b7980e36ca88fa53b479214c
salza323 <sal.zamora480@gmail.com>
2021-02-24 12:56:10 -0700

    added endpoing for updating a post

--- models/post.js
+++ models/post.js
@@ -1,129 +1,139 @@
 const database = require('../database/dbConfig');
 
 // Create post
-const createPost = post => {
-    return database('posts').insert(post).returning('id');
+const createPost = (post) => {
+  return database('posts').insert(post).returning('id');
 };
 
 const createRoomPostEntry = (post_id, room_id) => {
-    return database('rooms_to_posts').insert({ post_id, room_id });
+  return database('rooms_to_posts').insert({ post_id, room_id });
 };
 
 // Add entry for post like
 const addPostLike = (userID, postID) => {
-    return database('liked_posts').insert({ user_id: userID, post_id: postID });
+  return database('liked_posts').insert({ user_id: userID, post_id: postID });
 };
 
 // Fetch individual post
-const fetch = postID => {
-    return database('posts')
-		.join('users', 'posts.user_id', 'users.id')
-		.where('posts.id', postID)
-		.select([
-			'posts.id',
-			'users.id as user_id',
-			'users.profile_picture',
-			'users.display_name',
-			'posts.title',
-			'posts.description',
-			'posts.likes',
-			'posts.comments',
-			'posts.created_at',
-			'posts.updated_at'
-		])
-		.first();
+const fetch = (postID) => {
+  return database('posts')
+    .join('users', 'posts.user_id', 'users.id')
+    .where('posts.id', postID)
+    .select([
+      'posts.id',
+      'users.id as user_id',
+      'users.profile_picture',
+      'users.display_name',
+      'posts.title',
+      'posts.description',
+      'posts.likes',
+      'posts.comments',
+      'posts.created_at',
+      'posts.updated_at',
+    ])
+    .first();
 };
 
 // Fetch all posts
 // This is where search and sorting will occur
 const fetchRecent = () => {
-	return database('posts')
-		.join('users', 'posts.user_id', 'users.id')
-		.orderBy('posts.created_at', 'desc')
-		.select([
-			'posts.id',
-			'users.id as user_id',
-			'users.profile_picture',
-			'users.display_name',
-			'posts.title',
-			'posts.description',
-			'posts.likes',
-			'posts.comments',
-			'posts.created_at',
-			'posts.updated_at'
-		]);
+  return database('posts')
+    .join('users', 'posts.user_id', 'users.id')
+    .orderBy('posts.created_at', 'desc')
+    .select([
+      'posts.id',
+      'users.id as user_id',
+      'users.profile_picture',
+      'users.display_name',
+      'posts.title',
+      'posts.description',
+      'posts.likes',
+      'posts.comments',
+      'posts.created_at',
+      'posts.updated_at',
+    ]);
 };
 
 // Fetch all posts based on popularity
 const fetchPopular = () => {
-	return database('posts')
-		.join('users', 'posts.user_id', 'users.id')
-		.orderBy('posts.likes', 'desc')
-		.select([
-			'posts.id',
-			'users.id as user_id',
-			'users.profile_picture',
-			'users.display_name',
-			'posts.title',
-			'posts.description',
-			'posts.likes',
-			'posts.comments',
-			'posts.created_at',
-			'posts.updated_at'
-		]);
+  return database('posts')
+    .join('users', 'posts.user_id', 'users.id')
+    .orderBy('posts.likes', 'desc')
+    .select([
+      'posts.id',
+      'users.id as user_id',
+      'users.profile_picture',
+      'users.display_name',
+      'posts.title',
+      'posts.description',
+      'posts.likes',
+      'posts.comments',
+      'posts.created_at',
+      'posts.updated_at',
+    ]);
 };
 
-const fetchSearch = search => {
-	return database('posts')
-		.join('users', 'posts.user_id', 'users.id')
-		.whereRaw('LOWER(posts.title) LIKE ?', [`%${search}%`])
-		.orWhereRaw('LOWER(posts.description) LIKE ?', [`%${search}%`])
-		.select([
-			'posts.id',
-			'users.id as user_id',
-			'users.profile_picture',
-			'users.display_name',
-			'posts.title',
-			'posts.description',
-			'posts.likes',
-			'posts.comments',
-			'posts.created_at',
-			'posts.updated_at'
-		]);
+const fetchSearch = (search) => {
+  return database('posts')
+    .join('users', 'posts.user_id', 'users.id')
+    .whereRaw('LOWER(posts.title) LIKE ?', [`%${search}%`])
+    .orWhereRaw('LOWER(posts.description) LIKE ?', [`%${search}%`])
+    .select([
+      'posts.id',
+      'users.id as user_id',
+      'users.profile_picture',
+      'users.display_name',
+      'posts.title',
+      'posts.description',
+      'posts.likes',
+      'posts.comments',
+      'posts.created_at',
+      'posts.updated_at',
+    ]);
 };
 
-const incrementPostLikes = postID => {
-	return database('posts').where('id', postID).increment('likes', 1);
+const incrementPostLikes = (postID) => {
+  return database('posts').where('id', postID).increment('likes', 1);
 };
 
-const decrementPostLikes = postID => {
-	return database('posts').where('id', postID).decrement('likes', 1);
+const decrementPostLikes = (postID) => {
+  return database('posts').where('id', postID).decrement('likes', 1);
 };
 
-const incrementCommentCount = postID => {
-    return database('posts').where('id', postID).increment('comments', 1);
+const incrementCommentCount = (postID) => {
+  return database('posts').where('id', postID).increment('comments', 1);
 };
 
-const decrementCommentCount = postID => {
-    return database('posts').where('id', postID).decrement('comments', 1);
+const decrementCommentCount = (postID) => {
+  return database('posts').where('id', postID).decrement('comments', 1);
 };
 
 // Remove entry for post like
 const removePostLike = (userID, postID) => {
-    return database('liked_posts').where({ user_id: userID, post_id: postID }).del();
+  return database('liked_posts')
+    .where({ user_id: userID, post_id: postID })
+    .del();
 };
 
+//helper for updating a post with given postID and newDescription
+const postUpdate = (postID, newDescription) => {
+  return database('posts')
+    .where('id', postID)
+    .update({ description: newDescription })
+    .returning('*');
+};
 module.exports = {
-    createPost,
-    createRoomPostEntry,
-	addPostLike,
-	fetch,
-	fetchRecent,
-	fetchPopular,
-	fetchSearch,
-	incrementPostLikes,
-	decrementPostLikes,
-	incrementCommentCount,
-	decrementCommentCount,
-	removePostLike,
+  createPost,
+  createRoomPostEntry,
+  addPostLike,
+  fetch,
+  fetchRecent,
+  fetchPopular,
+  fetchSearch,
+  incrementPostLikes,
+  decrementPostLikes,
+  incrementCommentCount,
+  decrementCommentCount,
+  removePostLike,
+  postUpdate,
 };
--- routes/post.js
+++ routes/post.js
@@ -1,115 +1,138 @@
 const express = require('express');
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const { response } = require('express');
 
 const app = express.Router();
 
 // Create post
 app.post('/create', (request, response) => {
-    const userID = request.user.id;
-    const { title, description, room_id } = request.body;
-    
-    Post.createPost({ user_id: userID, title, description })
-        .then(([res]) => {
-            console.log(res);
-            Post.createRoomPostEntry(res, room_id)
-                .then(() => {
-                    response.status(200).json({ message: 'Post created successfully' });
-                })
-                .catch(err => {
-                    console.log(err);
-                    response.status(500).json({ message: 'Error creating post' });
-                });
+  const userID = request.user.id;
+  const { title, description, room_id } = request.body;
+
+  Post.createPost({ user_id: userID, title, description })
+    .then(([res]) => {
+      console.log(res);
+      Post.createRoomPostEntry(res, room_id)
+        .then(() => {
+          response.status(200).json({ message: 'Post created successfully' });
         })
-        .catch(err => {
-            console.log(err);
-            response.status(500).json({ message: 'Error creating post' });
+        .catch((err) => {
+          console.log(err);
+          response.status(500).json({ message: 'Error creating post' });
         });
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({ message: 'Error creating post' });
+    });
 });
 
 // Fetch single post
 app.get('/:id', (request, response) => {
-    const postID = request.params.id;
+  const postID = request.params.id;
 
-	Post.fetch(postID)
-		.then(post => response.status(200).json(post))
-		.catch(error => {
-			console.log(error);
-			response.status(500).json({ message: 'Error fetching individual post' });
-		});
+  Post.fetch(postID)
+    .then((post) => response.status(200).json(post))
+    .catch((error) => {
+      console.log(error);
+      response.status(500).json({ message: 'Error fetching individual post' });
+    });
 });
 
 // Fetch recent posts, not paginated yet
 app.post('/recent', (request, response) => {
-    Post.fetchRecent()
-        .then(res => response.status(200).json(res))
-        .catch(err => {
-            console.log(err);
-            response.status(500).json({message: 'Error fetching recent posts'});
-        });
+  Post.fetchRecent()
+    .then((res) => response.status(200).json(res))
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({ message: 'Error fetching recent posts' });
+    });
 });
 
 // Fetch posts by popularity
 app.post('/popular', (request, response) => {
-    Post.fetchPopular()
-        .then(res => response.status(200).json(res))
-        .catch(err => {
-            console.log(err);
-            response.status(500).json({message: 'Error fetching popular posts'});
-        });
+  Post.fetchPopular()
+    .then((res) => response.status(200).json(res))
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({ message: 'Error fetching popular posts' });
+    });
 });
-
 
 // Searches
 app.post('/search', (request, response) => {
-    const search = request.body.search;
-    
-    Post.fetchSearch(search)
-        .then(res => response.status(200).json(res))
-        .catch(err => {
-            console.log(err);
-            response.status(500).json({message: 'Error fetching posts'});
-        });
+  const search = request.body.search;
+
+  Post.fetchSearch(search)
+    .then((res) => response.status(200).json(res))
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({ message: 'Error fetching posts' });
+    });
 });
 
 // Like post
 app.get('/like/:id', (request, response) => {
-    const userID = request.user.id;
-    const postID = request.params.id;
+  const userID = request.user.id;
+  const postID = request.params.id;
 
-    Post.incrementPostLikes(postID)
-        .then(res => {
-            Post.addPostLike(userID, postID)
-                .then(r => response.status(200).json({ message: 'Post liked successfully' }))
-                .catch(e => {
-                    console.log(e);
-                    response.status(500).json({ message: 'Error adding post like' });
-                });
-        })
-        .catch(err => {
-            console.log(err);
-            response.status(500).json({ message: 'Error incrementing post\'s comment count' });
+  Post.incrementPostLikes(postID)
+    .then((res) => {
+      Post.addPostLike(userID, postID)
+        .then((r) =>
+          response.status(200).json({ message: 'Post liked successfully' })
+        )
+        .catch((e) => {
+          console.log(e);
+          response.status(500).json({ message: 'Error adding post like' });
         });
+    })
+    .catch((err) => {
+      console.log(err);
+      response
+        .status(500)
+        .json({ message: "Error incrementing post's comment count" });
+    });
 });
 
 // Unlike post
 app.delete('/like/:id', (request, response) => {
-    const userID = request.user.id;
-    const postID = request.params.id;
+  const userID = request.user.id;
+  const postID = request.params.id;
 
-    Post.decrementPostLikes(postID)
-        .then(res => {
-            Post.removePostLike(userID, postID)
-                .then(r => response.status(200).json({ message: 'Post unliked successfully' }))
-                .catch(e => {
-                    console.log(e);
-                    response.status(500).json({ message: 'Error removing post like' });
-                });
-        })
-        .catch(err => {
-            console.log(err);
-            response.status(500).json({ message: 'Error decrementing post\'s comment count' });
+  Post.decrementPostLikes(postID)
+    .then((res) => {
+      Post.removePostLike(userID, postID)
+        .then((r) =>
+          response.status(200).json({ message: 'Post unliked successfully' })
+        )
+        .catch((e) => {
+          console.log(e);
+          response.status(500).json({ message: 'Error removing post like' });
         });
+    })
+    .catch((err) => {
+      console.log(err);
+      response
+        .status(500)
+        .json({ message: "Error decrementing post's comment count" });
+    });
+});
+
+//Update a post must be the user that created post
+app.put('/update/:id', (request, response) => {
+  const postID = request.params.id;
+
+  const { newDescription } = request.body;
+  if (postID !== request.user.id) {
+    response.status(401).json({ message: 'unathorized user' });
+  }
+  Post.postUpdate(postID, newDescription)
+    .then((data) => response.status(200).json(data))
+    .catch((err) => {
+      response.status(500).json(err);
+    });
 });
 
 module.exports = app;
