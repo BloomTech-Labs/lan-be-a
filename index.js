e332860399c5da3d26fb00bbf37f2cf686339c3b
James Lundin <info@thecodediver.com>
2021-02-24 20:52:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,14 +30,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',  'https://main.d37zm5ayhfot8q.amplifyapp.com');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use(
   session({
     // name: 'LAN',
