4279a5f7f0c8b578ca9a011f366076ec3d067f33
James Lundin <info@thecodediver.com>
2021-02-25 09:50:03 -0500

    Update comment.js
    updates double quotes to single quotes

--- models/comment.js
+++ models/comment.js
@@ -1,8 +1,8 @@
-const database = require("../database/dbConfig");
+const database = require('../database/dbConfig');
 
 // Add a comment
 const add = (userID, postID, comment) => {
-  return database("comments")
+  return database('comments')
     .insert({ user_id: userID, post_id: postID, comment: comment })
     .returning("*");
 };
@@ -14,7 +14,7 @@ const deleteComments = (id) => {
 
 // Add a comment like
 const addCommentLike = (userID, commentID) => {
-  return database("liked_comments").insert({
+  return database('liked_comments').insert({
     user_id: userID,
     comment_id: commentID,
   });
@@ -24,60 +24,60 @@ const addCommentLike = (userID, commentID) => {
 // Should this be in post model? Maybe not because the URI will end in /comment anyway.
 const fetchRecent = (postID) => {
   return (
-    database("comments")
-      .join("users", "comments.user_id", "users.id")
-      .where("post_id", postID)
-      .orderBy("comments.created_at", "desc")
+    database('comments')
+      .join('users', 'comments.user_id', 'users.id')
+      .where('post_id', postID)
+      .orderBy('comments.created_at', 'desc')
       // Check what else is available here
       // user.id or comments.user_id?, research Knex as keyword so that you can use user.id
       .select([
-        "comments.id",
-        "comments.user_id",
-        "users.display_name",
-        "users.profile_picture",
-        "users.track",
-        "comments.post_id",
-        "comments.comment",
-        "comments.likes",
-        "comments.created_at",
-        "comments.updated_at",
+        'comments.id',
+        'comments.user_id',
+        'users.display_name',
+        'users.profile_picture',
+        'users.track',
+        'comments.post_id',
+        'comments.comment',
+        'comments.likes',
+        'comments.created_at',
+        'comments.updated_at',
       ])
   );
 };
 
 const fetchPopular = (postID) => {
   return (
-    database("comments")
-      .join("users", "comments.user_id", "users.id")
-      .where("post_id", postID)
-      .orderBy("comments.likes", "desc")
+    database('comments')
+      .join('users', 'comments.user_id', 'users.id')
+      .where('post_id', postID)
+      .orderBy('comments.likes', 'desc')
       // Check what else is available here
       // user.id or comments.user_id?, research Knex as keyword so that you can use user.id
       .select([
-        "comments.id",
-        "comments.user_id",
-        "users.display_name",
-        "users.profile_picture",
-        "users.track",
-        "comments.post_id",
-        "comments.comment",
-        "comments.likes",
-        "comments.created_at",
-        "comments.updated_at",
+        'comments.id',
+        'comments.user_id',
+        'users.display_name',
+        'users.profile_picture',
+        'users.track',
+        'comments.post_id',
+        'comments.comment',
+        'comments.likes',
+        'comments.created_at',
+        'comments.updated_at',
       ])
   );
 };
 
 const incrementCommentLikes = (commentID) => {
-  return database("comments").where("id", commentID).increment("likes", 1);
+  return database('comments').where('id', commentID).increment('likes', 1);
 };
 
 const decrementCommentLikes = (commentID) => {
-  return database("comments").where("id", commentID).decrement("likes", 1);
+  return database('comments').where('id', commentID).decrement('likes', 1);
 };
 
 const removeCommentLike = (userID, commentID) => {
-  return database("liked_comments")
+  return database('liked_comments')
     .where({ user_id: userID, comment_id: commentID })
     .del();
 };
