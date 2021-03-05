95ebca275fd0184ab80fe2c429a0776c96495fe7
James Lundin <info@thecodediver.com>
2021-03-04 22:14:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.use(helmet());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Origin', 'https://main.d37zm5ayhfot8q.amplifyapp.com');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 app.use(
