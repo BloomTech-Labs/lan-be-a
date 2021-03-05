fb185325f125eee93342b332ae4db106a5b3055d
James Lundin <info@thecodediver.com>
2021-03-04 22:28:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ const app = express();
 const FRONTEND_URL =
   process.env.FRONTEND_DEPLOYED_URL || 'http://localhost:3000';
 const PORT = process.env.PORT || 5000;
-app.all('/*', function(req, res) {
+app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 app.use(express.json());
 app.use(helmet());
