cb29bc79f9dc559304500335865c0a8c878cf236
Corey Power <56807090+CPower1248@users.noreply.github.com>
2021-03-16 18:26:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,6 @@ const FRONTEND_URL =
   process.env.FRONTEND_DEPLOYED_URL || 'http://localhost:3000';
 const PORT = process.env.PORT || 5000;
 
-app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(express.json());
 app.use(helmet());
 
