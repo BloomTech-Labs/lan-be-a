5cba4c48c3bc23363f0da36be7a17654b373be93
corey-power <CPower1248@gmail.com>
2021-03-19 15:32:56 -0400

    Updated seed placeholders with reasons for flagging

--- database/seeds/07_flagged_reason.js
+++ database/seeds/07_flagged_reason.js
@@ -5,9 +5,12 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('flagged_reason').insert([
-        {id: 1, reason: 'Reason 1', weight: 1},
-        {id: 2, reason: 'Reason 2', weight: 2},
-        {id: 3, reason: 'Reason 3', weight: 3},
+        {reason: 'Spam', weight: 1},
+        {reason: 'Bullying or Harrasment', weight: 2},
+        {reason: 'Hate Speech or Symbols', weight: 3},
+        {reason: 'Nudity or Sexual Content', weight: 4},
+        {reason: 'I just dislike it', weight: 5},
+        {reason: 'Other', weight: 6},
       ]);
     });
 };
