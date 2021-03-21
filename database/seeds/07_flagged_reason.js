5ac223c4245aeed966292f3969eb1e5ce52a00d6
Corey Power <56807090+CPower1248@users.noreply.github.com>
2021-03-21 00:48:57 -0400

    Update 07_flagged_reason.js

--- database/seeds/07_flagged_reason.js
+++ database/seeds/07_flagged_reason.js
@@ -5,12 +5,12 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('flagged_reason').insert([
-        {reason: 'Spam', weight: 1},
+        {reason: 'Spam', weight: 2},
         {reason: 'Bullying or Harrasment', weight: 2},
-        {reason: 'Hate Speech or Symbols', weight: 3},
-        {reason: 'Nudity or Sexual Content', weight: 4},
-        {reason: 'I just dislike it', weight: 5},
-        {reason: 'Other', weight: 6},
+        {reason: 'Hate Speech or Symbols', weight: 2},
+        {reason: 'Nudity or Sexual Content', weight: 2},
+        {reason: 'I just dislike it', weight: 1},
+        {reason: 'Other', weight: 1},
       ]);
     });
 };
